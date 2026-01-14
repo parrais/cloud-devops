e520cffa65b39c25767a73722c183e82688504fa
parrais <github@parrais.co.uk>
2026-01-14 16:52:38 +0000

    Add next week folder

---
+++ global-first-week/terraform-intro/main.tf
@@ -0,0 +1,15 @@
+# provider block
+provider "aws" {
+    # region
+    region = "eu-west-1"
+}
+
+data "aws_caller_identity" "current" {}
+ 
+output "aws_account_id" {
+    value = data.aws_caller_identity.current.account_id
+}
+ 
+output "aws_user_arn" {
+    value = data.aws_caller_identity.current.arn
+}
